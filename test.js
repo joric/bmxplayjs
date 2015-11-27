dac0e42f64075b2e3cc89c264bffc73a3149102b
joric <joric@users.sourceforge.net>
2015-11-28 00:07:18 +0500

    fixed codestyle and typos

--- test.js
+++ test.js
@@ -1,18 +1,15 @@
 var fs = require('fs');
 var BmxPlay = require('./src/BmxPlay.js');
-
 var p = new BmxPlay();
-
 var songName = 'default.bmx';
-
-p.Load(fs.readFileSync('./songs/'+songName).toString('binary'));
 
 p.SetCallback(function(e) {
 	process.stderr.write( 'Rendering: ' + ~~((e.pos+1)*100/e.size) + '%\r');
 });
 
+p.Load(fs.readFileSync('./songs/'+songName).toString('binary'));
+
 p.Render(function(data) {
-	var filename = songName.replace(".bmx",".wav");
-	fs.writeFileSync(filename, new Buffer(data.buffer));
+	fs.writeFileSync(songName.replace(".bmx",".wav"), new Buffer(data.buffer));
 });
 
